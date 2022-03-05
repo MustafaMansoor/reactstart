200a0b893b3b40d5fb491999cc0f55b684904a7b
MustafaMansoor <mustafamansoor47@gmail.com>
2022-03-05 13:27:10 +0500

    updated

---
+++ reactstartup/README.md
@@ -0,0 +1,3 @@
+https://github.com/MuhammadMohsin/PanacloudBootcamp2020/tree/session_4
+https://github.com/aamirpinger/bootcamp-2020
+https://daveceddia.com/react-getting-started-tutorial/?fbclid=IwAR0NzHXjI38xaf5mhC5ds1_EOPl5CVoquEQ7mXU0fSmS6BxsF3eix8i1OFc
---
+++ reactstartup/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Dinner from './dinner';
+
+function App() {
+    return ( <
+        div >
+        <
+        h1 >
+        soo jao allah kay bandon <
+        /h1> <
+        Dinner duaName = "ameen" / >
+        <
+        /div>
+    )
+}
+export default App;
